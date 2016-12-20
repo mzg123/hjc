efb019ed28f768cceb153bab7d7a7e24f32b9420
苗志高 <miaozhigao@yingu.com>
2016-12-20 16:43:35 +0800

    start
    start

---
+++ react/redux/actions.js
@@ -0,0 +1,36 @@
+
+
+module.exports={
+    getAjaxLog:function(subreddit) {
+        return function (dispatch) {
+            dispatch({ type: "getdata"
+                       ,param:subreddit
+            })
+            setInterval(function(){
+                dispatch({ type: "getdata"
+                    ,param:subreddit
+                    ,data:new Date().getSeconds()
+                })
+            },10000);
+            $.ajax({
+                type: "post",
+                url: "http://172.24.132.49:10280/get_data",
+                data: {
+                    name:"/data/logs/tomcat/tomcat-web.log"
+                    ,Remode:""
+                    ,Rows_count:"1"
+                    ,search_conditions:""
+                },
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                },
+                exception: function (data) {
+                    alert(9009);
+                    console.log(data);
+                }
+
+            });
+        }
+    }
+}
---
+++ react/redux/reducer.js
@@ -0,0 +1,77 @@
+
+var inits=function initState(){
+
+    return {
+        state:1,
+        content:{},
+        currenttxt:"",
+        treeItems:[
+            {
+                text:"1第一级"
+
+                ,child:[
+                {
+                    text:"1-1第二级"
+                    ,child:[
+                    {
+                        text:"1-1-1第三级"
+                        ,child:[
+                        {text:"1-1-1-1第四级"}
+                        ,{text:"1-1-1-2第四级"}
+                    ]
+                    },
+                    {
+                        text:"1-1-2第三级"
+                    }
+                ]
+                },
+                {
+                    text:"1-2第二级",
+                    child:[
+                        {
+                            text:"1-2-1第三级"
+                        }
+                        ,{
+                            text:"1-2-2第三级"
+                        }
+                    ]
+                }
+            ]
+            }
+        ]
+        ,options:{
+        }
+    }
+}
+
+module.exports={
+    initState:inits
+    ,counter:function (state,action) {
+    state||(state={mzg:"mzg"});
+    switch (action.type) {
+        case 'INCREMENT':
+            return state;
+        case 'DECREMENT':
+            return state;
+        default:
+            return state;
+    }
+}
+,treeCounter:function (state , action) {
+    state||(state=inits());
+
+    switch (action.type) {
+        case "loadding":
+            state.state=0;
+            return $.extend({},state);
+        case "getdata":
+            state.content[action.param]? state.content[action.param]= action.data+"\n\n"+state.content[action.param]: state.content[action.param]=action.data;
+            state.content.currentcontent=state.content[action.param];
+            state.currenttxt=state.content[action.param];
+            return $.extend({},state);
+        default:
+
+            return  state;
+    }
+}
+}
