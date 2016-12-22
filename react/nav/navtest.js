5ba87dd3e85948354fe731a31d2a79cef75a8c9d
苗志高 <miaozhigao@yingu.com>
2016-12-22 18:04:22 +0800

    嵌套路由
    嵌套路由

--- react/nav/navtest.js
+++ react/nav/navtest.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {Link} from 'react-router';
 //var Link=reactRouter.Link;
-export default class Navtest  extends React.Component{
+export default class navtest  extends React.Component{
     constructor(){
         super();
     }
     render(){
         return (
-              <Link to="/roller">轮播图</Link>
+            <div className="con"> <Link to="/roller">轮播图</Link> <Link to="/home">主页</Link>
+                  <Link to="/m">测试</Link>
+            {this.props.children}
+            </div>
         );
     }
 }
--- webpackbuildjs/react/tree_react.js
+++ webpackbuildjs/react/tree_react.js
@@ -7,30 +7,31 @@ var reduxStore=require('../../react/redux/store.js');
 var Tree=require("../../react/tree_react/tree.js");
 var Roller=require("../../react/roller/roller.js");
 var Test=require("../../react/tree_react/Test.js");
-import Navtest from "../../react/nav/navtest.js";
+import navtest from "../../react/nav/navtest.js";
 
 //var  App=require("../block/tree_react/reactredux.js");
-var Router=reactRouter.Router, Route=reactRouter.Route, browserHistory=reactRouter.browserHistory;
+var Router=reactRouter.Router, Route=reactRouter.Route,IndexRoute=reactRouter.IndexRoute, browserHistory=reactRouter.browserHistory;
 
 reduxStore.initState();
 
 
 
-
-console.log(Navtest)
-ReactDOM.render(
-    <Router history={browserHistory}>
-        <Route path="/nav"  component={Navtest}/>
-    </Router>
-    , document.getElementById("nav"));
+//ReactDOM.render(
+//    <Router history={browserHistory}>
+//        <Route path="/"  component={navtest}/>
+//    </Router>
+//    , document.getElementById("nav"));
 
 function d(){console.log(3);}
 ReactDOM.render(
     <Provider store={reduxStore}>
         <Router history={browserHistory}>
-            <Route path="/m"  onLeave={d} component={Test}/>
-            <Route path="/roller"   component={Roller} />
-            <Route path="/home"  onLeave={d} onEnter={d} component={Tree} />
+            <Route path="/"  component={navtest}>
+                <IndexRoute component={Roller}/>
+                <Route path="m"  onLeave={d} component={Test}/>
+                <Route path="roller"   component={Roller} />
+                <Route path="home"  onLeave={d} onEnter={d} component={Tree} />
+            </Route>
         </Router>
     </Provider>
     , document.getElementById("con"));
